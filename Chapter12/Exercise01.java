cf4b95dee29969d6e322907bbd30a0ed614eda2a
Mr. Young <masyoung@hawaii.edu>
2020-08-04 01:12:46 -1000

    Completed Exericse 1

---
+++ Chapter12/Exercise01.java
@@ -0,0 +1,43 @@
+/** A simple calculator that does arithmetic on just 2 integers. */
+public class Exercise01 {
+  /** The main method. */
+  public static void main(String[] args) {
+    // Check number of strings passed
+    if (args.length != 3) {
+      System.out.println("Usage: java Exercise01 operand1 operator operand2");
+      System.exit(0);
+    }
+
+   int operand1 = 1;
+   int operand2 = 1;
+   // Attempt to convert operand1 to an int
+   try {
+      operand1 = Integer.parseInt(args[0]);
+    } catch (NumberFormatException ex) {
+      System.out.printf("Wrong Input: %s\n", args[0]);
+      System.exit(0);
+    }
+    // Attempt tp convert operand2 to an int
+    try {
+      operand2 = Integer.parseInt(args[2]);
+    } catch (NumberFormatException ex) {
+      System.out.printf("Wrong Input: %s\n", args[2]);
+      System.exit(0);
+    }
+
+    // Compute problem
+    int result = 0;
+    switch (args[1].charAt(0)) {
+      case '+': result = operand1 + operand2;
+                break;
+      case '-': result = operand1 - operand2;
+                break;
+      case '.': result = operand1 * operand2;
+                break;
+      case '/': result = operand1 / operand2;
+    }
+
+    // Print results
+    System.out.printf("%d %s %d = %d\n", operand1, args[1], operand2, result);
+  }
+}
