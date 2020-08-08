65523c7e108857667d23ef1e5ed020a7023d2b32
Mr. Young <masyoung@hawaii.edu>
2020-08-07 20:48:19 -1000

    Completed Exercise 12.6

---
+++ Chapter12/Exercise06.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+/** Converts a hexadecimal to a decimal. */
+public class Exercise06 {
+  /** Main method */
+  public static void main(String[] args) {
+    // Create a Scanner
+    Scanner input = new Scanner(System.in);
+
+    // Prompt the user to enter a string
+    System.out.print("Enter a hex number: ");
+    String hex = input.nextLine();
+    
+    // Display result
+    System.out.println("The decimal value for hex number "
+      + hex + " is " + hexToDecimal(hex.toUpperCase()));
+  }
+  
+
+  /** Converts hexadecimal to decimal.
+      @param hex The hexadecimal
+      @return The deciaml value of hex
+      @throws NumberFormatException if hex is not a hexadecimal
+    */
+  public static int hexToDecimal(String hex) throws NumberFormatException {
+    // Check if hex is a hexadecimal. Throw Exception if not.
+    boolean patternMatch = Pattern.matches("[0-9A-F]+", hex);
+    if (!patternMatch) 
+      throw new NumberFormatException();
+
+    // Convert hex to a decimal
+    int decimalValue = 0;
+    for (int i = 0; i < hex.length(); i++) {
+      char hexChar = hex.charAt(i);
+      decimalValue = decimalValue * 16 + hexCharToDecimal(hexChar);
+    }
+    // Return the decimal
+    return decimalValue;
+  }
+  
+  
+  /** Converts a hexadecimal Char to a deciaml.
+      @param ch The hexadecimal Char
+      @return The decimal value of ch
+    */
+  public static int hexCharToDecimal(char ch) {
+    if (ch >= 'A' && ch <= 'F')
+      return 10 + ch - 'A';
+    else // ch is '0', '1', ..., or '9'
+      return ch - '0';
+  }
+}
